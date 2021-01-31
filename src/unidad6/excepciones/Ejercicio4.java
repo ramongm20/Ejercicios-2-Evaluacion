eb897d50bb74ba5457c4a7af9c0f9a45892e72e0
Ramón García <ramongm20@educastur.es>
2021-01-31 15:42:26 +0100

    Ejercicio 4 de la unidad 6 (excepciones) resuelto

---
+++ src/unidad6/excepciones/Ejercicio4.java
@@ -0,0 +1,64 @@
+package unidad6.excepciones;
+
+public class Ejercicio4 {
+	 
+	public static void main(String[] args) { 
+	try { 
+	System.out.println("Antes de metodoA"); 
+	metodoA(); 
+	System.out.println("Despues de metodoA"); 
+	} catch (Exception e) { 
+	System.out.println("main: " + e); 
+	} finally { 
+	System.out.println("main: finally"); 
+	} 
+	} // cierra main 
+	 
+	public static void metodoA() { 
+	try { 
+	System.out.println("Antes de metodoB"); 
+	metodoB(); 
+	System.out.println("Despues de metodoB"); 
+	} catch (Exception e) { 
+	System.out.println("metodoA: " + e); 
+	} finally { 
+	System.out.println("metodoA: finally"); 
+	} 
+	} // cierra metodoA
+	 
+	public static void metodoB() { 
+	try { 
+	System.out.println("Antes de metodoC"); 
+	metodoC(); 
+	System.out.println("Despues de metodoC"); 
+	} catch (ArrayIndexOutOfBoundsException e) { 
+	System.out.println("metodoB: " + e); 
+	} finally { 
+	System.out.println("metodoB: finally"); 
+	} 
+	} // cierra metodoB
+	 
+	public static void metodoC() { 
+	try { 
+	System.out.println("Antes de metodoD"); 
+	metodoD(); 
+	System.out.println("Despues de metodoD"); 
+	} catch (NumberFormatException e) { 
+	System.out.println("metodoC: " + e); 
+	} finally { 
+	System.out.println("metodoC: finally"); 
+	} 
+	} // cierra metodoC
+	
+	public static void metodoD() { 
+	try { 
+	int a[] = new int[4]; 
+	a[a.length] = a.length; 
+	} catch (ClassCastException e) { 
+	System.out.println("metodoD: " + e); 
+	} finally { 
+	System.out.println("metodoD: finally"); 
+	} 
+	} // cierra metodoD
+	 
+}
